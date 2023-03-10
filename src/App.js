5d498dd56cd35f946635447ec08bf663d73c7ecf
marynmcnamara <119798224+marynmcnamara@users.noreply.github.com>
2023-03-10 14:05:49 -0500

    add github link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Maryn McNamara and is
+        <a
+          href="https://github.com/marynmcnamara/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
